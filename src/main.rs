75ff511dc438929353ed5efdaad4339a4509e871
Amos Wenger <amoswenger@gmail.com>
2023-03-05 20:38:22 +0100

    wait & print every syscall, add syscall.json

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 use std::{os::unix::process::CommandExt, process::Command};
 
-fn main() {
-    println!("I'm dad, my PID is {}", std::process::id());
+use nix::{
+    sys::{ptrace, wait::waitpid},
+    unistd::Pid,
+};
+use owo_colors::OwoColorize;
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut command = Command::new("cat");
     command.arg("/etc/hosts");
     unsafe {
         command.pre_exec(|| {
-            println!("I'm squid, my PID is {}", std::process::id());
-            Ok(())
+            use nix::sys::ptrace::traceme;
+            traceme().map_err(|e| e.into())
         });
     }
 
-    let mut child = command.spawn().unwrap();
-    child.wait().unwrap();
+    let child = command.spawn()?;
+    let child_pid = Pid::from_raw(child.id() as _);
+    let res = waitpid(child_pid, None)?;
+    eprintln!("first wait: {:?}", res.yellow());
+
+    loop {
+        ptrace::syscall(child_pid, None)?;
+        _ = waitpid(child_pid, None)?;
+        let regs = ptrace::getregs(child_pid)?;
+        eprintln!(
+            "syscall {} called with {:x}, {:x}, {:x}, returned {:x}",
+            regs.orig_rax.green(),
+            regs.rdi.blue(),
+            regs.rsi.blue(),
+            regs.rdx.blue(),
+            regs.rax.yellow(),
+        );
+    }
 }
