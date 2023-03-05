f76bae32d5cccfd70c06b1f088177b37c1bcc15c
Amos Wenger <amoswenger@gmail.com>
2023-03-05 18:46:45 +0100

    print PID

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::process::Command;
+use std::{os::unix::process::CommandExt, process::Command};
 
 fn main() {
+    println!("I'm dad, my PID is {}", std::process::id());
+
     let mut command = Command::new("cat");
     command.arg("/etc/hosts");
+    unsafe {
+        command.pre_exec(|| {
+            println!("I'm squid, my PID is {}", std::process::id());
+            Ok(())
+        });
+    }
 
     let mut child = command.spawn().unwrap();
     child.wait().unwrap();
