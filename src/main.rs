9a9db4634ad2fb4b6e5f01618c14cc81ca948e11
Amos Wenger <amoswenger@gmail.com>
2023-03-06 02:11:09 +0100

    don't double-print, use syscall json table

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{os::unix::process::CommandExt, process::Command};
+use std::{collections::HashMap, os::unix::process::CommandExt, process::Command};
 
 use nix::{
     sys::{ptrace, wait::waitpid},
@@ -7,6 +7,19 @@ use nix::{
 use owo_colors::OwoColorize;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let json: serde_json::Value = serde_json::from_str(include_str!("syscall.json"))?;
+    let syscall_table: HashMap<u64, String> = json["aaData"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|item| {
+            (
+                item[0].as_u64().unwrap(),
+                item[1].as_str().unwrap().to_owned(),
+            )
+        })
+        .collect();
+
     let mut command = Command::new("cat");
     command.arg("/etc/hosts");
     unsafe {
@@ -21,17 +34,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let res = waitpid(child_pid, None)?;
     eprintln!("first wait: {:?}", res.yellow());
 
+    let mut is_sys_exit = false;
     loop {
         ptrace::syscall(child_pid, None)?;
         _ = waitpid(child_pid, None)?;
-        let regs = ptrace::getregs(child_pid)?;
-        eprintln!(
-            "syscall {} called with {:x}, {:x}, {:x}, returned {:x}",
-            regs.orig_rax.green(),
-            regs.rdi.blue(),
-            regs.rsi.blue(),
-            regs.rdx.blue(),
-            regs.rax.yellow(),
-        );
+        if is_sys_exit {
+            let regs = ptrace::getregs(child_pid)?;
+            eprintln!(
+                "{}({:x}, {:x}, {:x}, ...) = {:x}",
+                syscall_table[&regs.orig_rax].green(),
+                regs.rdi.blue(),
+                regs.rsi.blue(),
+                regs.rdx.blue(),
+                regs.rax.yellow(),
+            );
+        }
+        is_sys_exit = !is_sys_exit;
     }
 }
